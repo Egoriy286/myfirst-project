1b91afbb4d0594ae9f94cbf49dd8a09001295992
Monastyrev Egoriy <98502409+Egoriy286@users.noreply.github.com>
2024-10-14 02:46:45 +0900

    deleted proxy

--- vite.config.ts
+++ vite.config.ts
@@ -18,17 +18,5 @@ export default defineConfig({
   server: {
     host: "0.0.0.0",
     port: 5200,
-    proxy: {
-      '/files': {
-        target: 'http://127.0.0.1:8000',
-        changeOrigin: true,
-        secure: false,
-      },
-      '/download': {
-        target: 'http://127.0.0.1:8000',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
   }
 })
