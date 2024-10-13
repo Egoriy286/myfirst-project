2458fa65bcb886f4a49fe16d63a270f7191166e6
Monastyrev Egoriy <98502409+Egoriy286@users.noreply.github.com>
2024-10-14 02:40:33 +0900

    gmail

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="contact-page">
     <h1>Контактная информация</h1>
-    <p><strong>Email:</strong> <a href="gmail:egoriy08122002@gmail.com">proste20@gmail.com</a></p>
+    <p><strong>Email:</strong> <a href="gmail:proste2002@gmail.com">proste2002@gmail.com</a></p>
     <p><strong>Телефон:</strong> +7 (965) 993-9320</p>
     <p><strong>Страна:</strong> Россия</p>
   </div>
